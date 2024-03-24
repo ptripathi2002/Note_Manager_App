1e8c5b081ec9406267e14006674a202fb1370c2e
Prashant Tripathi <90204816+ptripathi2002@users.noreply.github.com>
2024-03-25 00:44:47 +0530

    Update connectDB.js

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(
-      "mongodb://localhost:27017/task-manager-app"
+      "mongodb+srv://Prashant_1202:Prashant123_45@prashanttripathicluster.dqhx0g6.mongodb.net/Note_Manager_App?retryWrites=true&w=majority"
     );
 
     console.log(`Database Connected`);
